8a3bc1397ead3e5e75a70415f532776c38627505
Scott Caudle <scottcaudlewebsite@gmail.com>
2021-04-22 20:19:48 -0400

    first commit for career day

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,14 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+<input v-model="message" placeholder="type your name here!">
+
+<h3>Thanks for listening to my presentation</h3>
+<transition name="fade">
+  <h1 class="the-message">{{ message }}</h1>
+</transition>
+<h3 style="margin-top: 0;">Hope you have a great rest of your friday!</h3>
+
   </div>
 </template>
 
@@ -34,14 +16,29 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    message: String
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input {
+  font-family: 'Fredoka One', cursive;
+  height: 100px;
+  font-size: 30px;
+  text-align: center;
+  border-radius: 4px;
+  width: 50vw;
+}
+
+h1 {
+  font-family: 'Fredoka One', cursive;
+  font-size: 90px;
+}
 h3 {
+  font-family: 'Fredoka One', cursive;
+  font-size: 40px;
   margin: 40px 0 0;
 }
 ul {
@@ -54,5 +51,20 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.the-message {
+   display: inline-block;
+  animation: woah 2s linear infinite alternate;
+}
+
+@keyframes woah {
+   0% { color: grey; }
+  75% { color: black; }
+ 100% { transform: translateX(50%); color: purple; }
 }
 </style>
